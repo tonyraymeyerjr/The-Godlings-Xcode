add30fd6277c6d7f7ddc0df4fecdfd53b605bb4b
TonyMeyer <tony.meyer@testtracks.com>
2016-07-31 17:33:41 -0400

    Initial Commit

---
+++ The-Godlings-XcodeTests/The_Godlings_XcodeTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Godlings_XcodeTests.swift
+//  The-Godlings-XcodeTests
+//
+//  Created by Tony Meyer on 7/31/16.
+//  Copyright © 2016 Tony Meyer. All rights reserved.
+//
+
+import XCTest
+@testable import The_Godlings_Xcode
+
+class The_Godlings_XcodeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
